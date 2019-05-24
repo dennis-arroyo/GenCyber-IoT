9e5b7fded5cb5db0b2db55ec4746377f43573fe6
Dennis <dennis.o.arroyo@gmail.con>
2019-05-24 12:58:56 -0400

    actions

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,17 +1,38 @@
 package gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+
 import java.io.IOException;
 
 public class Controller {
 
+    @FXML
+    private TextField ipAddress;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Text ip;
+
+    public void setIpAddress() {
+        ip.setText(ipAddress.getText());
+    }
+
     public void connectButton(ActionEvent event) throws IOException {
         System.out.println("Changing to the credentials screen");
+        System.out.println(this.ipAddress.getText());
         sceneChange("credentials.fxml", event);
     }
 
@@ -20,9 +41,16 @@ public class Controller {
         sceneChange("home.fxml", event);
     }
 
+    public void loginButton(ActionEvent event) {
+        String username = this.username.getText();
+        String password = this.password.getText();
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+
     private void sceneChange(String fxmlName, ActionEvent event) throws IOException {
-        Parent crendetials_screen = FXMLLoader.load(getClass().getResource(fxmlName));
-        Scene credentials_screen_scene = new Scene(crendetials_screen);
+        Parent credentials_screen = FXMLLoader.load(getClass().getResource(fxmlName));
+        Scene credentials_screen_scene = new Scene(credentials_screen);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(credentials_screen_scene);
         stage.show();
